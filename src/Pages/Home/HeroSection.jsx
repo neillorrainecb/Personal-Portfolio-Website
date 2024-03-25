d7c5c45120dcec5780f2bbd0da6db6a42320337e
neillorrainecb <neillorrainecb@gmail.com>
2024-03-25 12:09:38 +0800

    add socials in responsive layout

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -17,6 +17,24 @@ export default function HeroSection (){
                     </p>
                 </div>
 
+                <div className="hero--socials">
+                    <a className="hero--section--socials-link" href="https://www.linkedin.com/in/neillorrainecb/">
+                    <i class="fa-brands fa-linkedin"></i>
+                    </a>
+
+                    <a className="hero--section--socials-link" href="https://github.com/neillorrainecb">              
+                    <i class="fa-brands fa-square-github"></i>
+                    </a>
+
+                    <a className="hero--section--socials-link" href="https://www.behance.net/neillorrainecb">
+                    <i class="fa-brands fa-square-behance"></i>
+                    </a>  
+
+                    <a className="hero--section--socials-link" href='mailto:neillorrainecb@gmail.com'>
+                    <i class="fa-solid fa-square-envelope"></i>
+                    </a>
+                </div>
+
                <a href = "./img/Resume.pdf" target="\_blank">
                     <button className="btn btn-primary">Resume</button>
                 </a>
@@ -25,5 +43,7 @@ export default function HeroSection (){
             <div className="hero--section--img">
                 <img src="./img/hero-img.png" alt="Hero Section" />
             </div>
+
+            
         </section>
 }
